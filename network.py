4aa8c381d8a0a8e0410ba50dea3047475343b5f2
yigitcelik <yigitcelik@gmx.com>
2022-09-11 17:03:34 +0300

    english column names

--- network.py
+++ network.py
@@ -12,12 +12,9 @@ import xlwings as xw
 data =  pd.read_excel('network_macro.xlsm')
 
 # %%
-data
-
-# %%
 def find_upper_assy(x):
    
-    assy=data.loc[(data['seviye']<x[0]) & (data.index<x.name),'malzeme'].tail(1).to_string(index=False)
+    assy=data.loc[(data['level']<x[0]) & (data.index<x.name),'material'].tail(1).to_string(index=False)
     if assy.isalpha():
         return assy
     else:
@@ -39,7 +36,7 @@ T.nodes() #view the nodes
 
 
 
-activities_edges = [(a,b) for a,b in data[["malzeme","next_assy"]].to_numpy()]
+activities_edges = [(a,b) for a,b in data[["material","next_assy"]].to_numpy()]
 
 T.add_edges_from(activities_edges)   #add edges
 
@@ -48,7 +45,7 @@ T.edges()
 
 # %%
 #add weights to the existing edges
-activities_weights = data["sure"].to_list()
+activities_weights = data["leadtime(day)"].to_list()
 i=0
 for s,d in T.edges():
     T[s][d]['weight'] =activities_weights[i]
@@ -64,7 +61,6 @@ nx.draw_networkx_edges(T,pos,arrows=True)
 nx.draw_networkx_edge_labels(T,pos,edge_labels=nx.get_edge_attributes(T,'weight'))
 nx.draw_networkx_labels(T,pos)
 plt.savefig('network.png',dpi=100)
-plt.show()
 
 # %%
 print(nx.shortest_path(T,'X','-',weight='weight'))
@@ -80,17 +76,17 @@ def find_short(x):
 
 # %%
 data["total_time"] = nx.dag_longest_path_length(T,weight='weight')
-data["self_time"] = data.apply(find_short,axis=1)
+data["impacted_time"] = data.apply(find_short,axis=1)
 data
 
 # %%
 today = dt.datetime.today().strftime("%Y-%m-%d")
-critic_index = data[data["self_time"]==data['self_time'].max()].index.tolist()[0]
+critic_index = data[data["impacted_time"]==data['impacted_time'].max()].index.tolist()[0]
 critic_list = nx.shortest_path(T,data.iloc[critic_index][1],'-',weight='weight')
 data["critical_path"]=''
-data.loc[(data.index <= critic_index) & data['malzeme'].isin(critic_list),'critical_path']='yes'
-#data = data.drop(['self_time'],axis=1)
-earliest_begin= max(data['baslangic tarihi'].min().strftime("%Y-%m-%d"),today)
+data.loc[(data.index <= critic_index) & data['material'].isin(critic_list),'critical_path']='yes'
+
+earliest_begin= max(data['start_date'].min().strftime("%Y-%m-%d"),today)
 earliest_begin = dt.datetime.strptime(earliest_begin,"%Y-%m-%d")
 calc_finish=  earliest_begin + timedelta(nx.dag_longest_path_length(T,weight='weight'))
 calc_finish
@@ -107,9 +103,6 @@ data['calc_start_date'] = data.apply(calc_start_date,axis=1)
 data['calc_finish_date'] = data.apply(calc_finish_date,axis=1)
 
 # %%
-data
-
-# %%
 wb = xw.Book("network_macro.xlsm")
 ws = wb.sheets["Sheet1"]
 
@@ -123,10 +116,10 @@ rownum= xw.Range('B1').current_region.last_cell.row  #find the lastest row of th
 
 xw.Range(f'B1:B{rownum}').color = None #remove the color of B column
 
-for cell in yes_list:
+for cell in yes_list:    #it changes the color to red in B column cells that are in the same index with critical path yes'es
     xw.Range(f"B{int(cell)+2}").color = '#D10000'
 
-ws.tables.add(ws.used_range, name="a_table",table_style_name = "TableStyleLight1")
+ws.tables.add(ws.used_range, name="a_table",table_style_name = "TableStyleLight1")  #add autofilter property to all columns
 ws.tables["a_table"].show_autofilter = True
 
 wb.save('network_macro.xlsm')
@@ -135,8 +128,8 @@ wb.save('network_macro.xlsm')
 import plotly.express as px
 
 colors= ['#D10000', '#45B08C']
-fig = px.timeline(data, x_start="calc_start_date", x_end="calc_finish_date", y="malzeme",color='critical_path',color_discrete_sequence=colors)
-fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+fig = px.timeline(data, x_start="calc_start_date", x_end="calc_finish_date", y="material",color='critical_path',color_discrete_sequence=colors)
+fig.update_yaxes(autorange="reversed") # makes the tasks are listed from up to down
 fig.update_layout()
 fig.write_html("gant.html")
 fig.show()
